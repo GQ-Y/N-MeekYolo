b330fa924da145e57bcdaae35ac0e74bdb138eef
hook <help@hook.design>
2025-04-07 14:16:36 +0800

    fix:定稿版1.0.0:未开发完成

--- analysis_service/docker/structure.sh
+++
@@ -1,8 +0,0 @@
-analysis_service/
-├── Dockerfile.cpu
-├── Dockerfile.gpu
-├── docker
-│   ├── entrypoint.sh
-│   └── healthcheck.sh
-├── requirements.txt
-└── docker-compose.yml 
--- api_service/docker/structure.sh
+++
@@ -1,8 +0,0 @@
-api_service/
-├── Dockerfile
-├── docker
-│   ├── entrypoint.sh
-│   ├── healthcheck.sh
-│   └── structure.sh
-├── requirements.txt
-└── docker-compose.yml
